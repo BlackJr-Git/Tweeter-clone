e7f567bab6e01238e82c3f1def3f6ab33fdd9daa
BlackJr-Git <juniorassosa@gmail.com>
2023-11-23 07:07:37 +0100

    V1 architecture with all small components

--- src/components/tweet-body.jsx
+++ src/components/tweet-body.jsx
@@ -1,13 +1,38 @@
 import { TweetImage, TweetText, TweetTitle } from "../components";
 
-function TweetBody() {
-    return (
-        <div className="tweet-body">
-            <TweetTitle />
-            <TweetText />
-            <TweetImage />
-        </div>
-    )
+function TweetBody({texte, imgsrc}) {
+
+    if (imgsrc){
+        return (
+            <div className="tweet-body">
+                
+                <TweetTitle />
+                <p className="tweet-text"> {texte} </p>
+                
+                <div className="tweet-image">
+                    <img src= {imgsrc} alt="" />
+                </div>
+                
+                {/* 
+                <TweetText />
+                <TweetImage /> */}
+            </div>
+        )
+    } else {
+        return (
+            <div className="tweet-body">
+                
+                <TweetTitle />
+                <p className="tweet-text"> {texte} </p>
+                
+                {/* 
+                <TweetText />
+                <TweetImage /> */}
+            </div>
+        )
+    }
+
+    
 }
 
 export default TweetBody ;
--- src/components/tweet-content.jsx
+++ src/components/tweet-content.jsx
@@ -4,8 +4,9 @@ import {TweetActions,TweetBody } from "../components"
 function TweetContent() {
     return (
         <div className="tweet-content">
-            <TweetBody />
+            <TweetBody imgsrc={"src\\images\\tweet-image.png"} texte={"Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land."} />
             <TweetActions />
+            
         </div>
     )
 }
