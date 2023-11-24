660f7fdbc6861fb3a08e61baa0ff584bf6e059db
BlackJr-Git <juniorassosa@gmail.com>
2023-11-24 15:51:04 +0100

    feat : refactor tweet content addind a condition on tweet images

--- src/components/tweet-content.jsx
+++ src/components/tweet-content.jsx
@@ -3,7 +3,7 @@ import {TweetAction } from "../components"
 
 function TweetContent({texte, imgsrc,author, userName, time , reply, retweet, react}) {
             
-    if (imgsrc){
+    
         return (
             
             <div className="tweet-content">
@@ -17,9 +17,10 @@ function TweetContent({texte, imgsrc,author, userName, time , reply, retweet, re
 
                     <p className="tweet-text"> {texte} </p>
                         
-                    <div className="tweet-image">
-                        <img src= {imgsrc} alt="" />
-                    </div>
+                    {imgsrc &&  <div className="tweet-image">
+                                    <img src= {imgsrc} alt="" />
+                                </div> 
+                    }
                 </div>
                 
                 <div className="tweet-actions">
@@ -29,29 +30,7 @@ function TweetContent({texte, imgsrc,author, userName, time , reply, retweet, re
                     <TweetAction imgsrc={"src\\images\\Icons\\Share.png"} actionCount={null} />
                 </div>
             </div>
-        )
-    } else {
-        return (
-            <div className="tweet-content">
-                <div className="tweet-body">
-                    
-                    <div className="tweet-title">
-                        <p className="tweet-title-author"> {author} </p>
-                        <img src="src\images\Icons\Verified.png" alt="verfied icon" />
-                        <p className="tweet-title-details">{userName} . {time}</p>
-                    </div>
-
-                    <p className="tweet-text"> {texte} </p>
-                </div>
-                
-                <div className="tweet-actions">
-                    <TweetAction imgsrc={"src\\images\\Icons\\Reply.png"} actionCount={reply} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\Retweet.png"} actionCount={retweet} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\React.png"} actionCount={react} />
-                    <TweetAction imgsrc={"src\\images\\Icons\\Share.png"} actionCount={null} />
-                </div>
-            </div>
-        ) }
+        );
 }  
 
 export default TweetContent ;
